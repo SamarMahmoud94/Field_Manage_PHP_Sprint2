9a938256e721b33d9babf9af664629b6f783ac8b
Nour Ahmed <nourahmedaziz03@gmail.com>
2025-08-30 17:43:41 +0300

    add fields, field slots and week days

--- routes/api.php
+++ routes/api.php
@@ -8,17 +8,19 @@ use App\http\Controllers\BrowseFieldsController;
 use App\Http\Controllers\ViewFieldDetailsController;
 use App\Http\Controllers\BookingFieldController;
 use App\Http\Controllers\Api\Auth\PasswordResetController;
-<<<<<<< HEAD
 use App\Http\Controllers\EnquiryController;
 use App\Http\Controllers\SettingController;
 use App\Http\Controllers\ViewTeamController;
 use App\Http\Controllers\ManagementReviewController;
 use App\Http\Controllers\ManagementEnquiryController;
+use App\Http\Controllers\CRUDFieldController;
+use App\Http\Controllers\FieldSlotController;
+use App\Http\Controllers\WeekDayController;
 
 //reset password 
 Route::post('/forgot-password', [PasswordResetController::class, 'forgotPassword']);
 Route::post('/reset-password', [PasswordResetController::class, 'resetPassword']);
-=======
+
 use App\Http\Controllers\UserController;
 //reset password 
 Route::post('/forgot-password', [PasswordResetController::class, 'forgotPassword']);
@@ -26,17 +28,13 @@ Route::post('/reset-password', [PasswordResetController::class, 'resetPassword']
 
 // activate and deavtivate 
 
-Route::middleware('auth:api')->group(function() {
+Route::middleware('auth:api')->group(function () {
     Route::post('/user/{id}/toggle-status', [App\Http\Controllers\Api\Auth\UserController::class, 'toggleStatus']);
 });
 
 
 // Route::middleware('auth:api')->post('/user/{id}/toggle-status', [UserController::class, 'toggleStatus']);
 
-
-
-// update profile 
->>>>>>> activate_deactivate_api
 
 // update profile 
 Route::middleware('auth:api')->group(function () {
@@ -76,10 +74,10 @@ Route::post('/settings', [SettingController::class, 'create'])->middleware('auth
 // Update Setting by admin route
 Route::put('/settings', [SettingController::class, 'update'])->middleware('auth:api');
 
-// View all teams
+// View all teams route
 Route::get('/teams', [ViewTeamController::class, 'viewAllTeams'])->middleware('auth:api');
 
-// View specific team using the team id
+// View specific team using the team id route
 Route::get('/teams/{id}', [ViewTeamController::class, 'viewTeam'])->middleware('auth:api');
 
 //Management Reviews
@@ -95,3 +93,37 @@ Route::middleware('auth:api')->delete('/DeleteReview', [ManagementReviewControll
 Route::middleware('auth:api')->get('/AllEnquiry', [ManagementEnquiryController::class, 'index']);
 //hide enquiry
 Route::middleware('auth:api')->post('/HideEnquiry', [ManagementEnquiryController::class, 'hide']);
+
+// CRUD Field by admin routes
+// Create field route
+Route::post('/fields', [CRUDFieldController::class, 'createField'])->middleware('auth:api');
+
+// View all fields route
+Route::get('/fields', [CRUDFieldController::class, 'viewAllField'])->middleware('auth:api');
+
+// View specific fild using field id route
+Route::get('/fields/{id}', [CRUDFieldController::class, 'viewField'])->middleware('auth:api');
+
+// Update field route
+Route::put('/fields/{id}', [CRUDFieldController::class, 'updateField'])->middleware('auth:api');
+
+// Delete field route
+Route::delete('/fields/{id}', [CRUDFieldController::class, 'deleteField'])->middleware('auth:api');
+
+// Display week day route
+Route::get('/weekdays', [WeekDayController::class, 'displayWeekDay']);
+
+// Create slot by admin route
+Route::post('/slots', [FieldSlotController::class, 'createSlot']);
+
+// View all slots by admin route
+Route::get('/slots', [FieldSlotController::class, 'viewAllSlots']);
+
+// View specific slot by admin route
+Route::get('/slots/{id}', [FieldSlotController::class, 'viewSlot']);
+
+// Update slot by admin route
+Route::put('/slots/{id}', [FieldSlotController::class, 'updateSlot']);
+
+// Delete slot by admin route
+Route::delete('/slots/{id}', [FieldSlotController::class, 'deleteSlot']);
